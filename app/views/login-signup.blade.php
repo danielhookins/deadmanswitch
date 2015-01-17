269df9198186974cd60d9803ae9a75b730c8bcd3
Daniel Hookins <daniel@hookins.net>
2015-01-17 20:32:42 +1030

    Add login-signup template

--- app/views/login-signup.blade.php
+++ app/views/login-signup.blade.php
@@ -5,20 +5,72 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Dead Man Switch - Login or Sign up</title>
-		<link href="{{ URL::asset('_/css/bootstrap.css') }}" rel="stylesheet">
-		<link href="{{ URL::asset('_/css/login-signup.css') }}" rel="stylesheet">
-		<link href="{{ URL::asset('http://fonts.googleapis.com/css?family=Lato|Alegreya:400,700') }}" rel='stylesheet' type='text/css'>
+		<link href="{{ asset('_/css/bootstrap.css') }}" rel="stylesheet">
+		<link href="{{ asset('_/css/login-signup.css') }}" rel="stylesheet">
+		<link href="{{ asset('http://fonts.googleapis.com/css?family=Lato|Alegreya:400,700') }}" rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<div>
-			<h2>Login</h2>
-		</div>
-		<div>
-			<h2>I'm new. Sign up</h2>
-		</div>
+		<div class="wrapper">
+			<!-- TOP-BAR -->
+			<section id="top-bar">
+
+			</section> <!-- /.TOP-BAR -->
+
+			<!-- MAIN -->
+			<section id="main">
+				<div class="container main-content">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="col-md-8 col-md-offset-4 welcome-box">
+								<h2>Welcome to the app</h2>
+								<p>Bacon ipsum dolor amet prosciutto chuck beef jowl pig boudin. Turkey bresaola ball tip prosciutto turducken tongue shoulder drumstick pork chop strip steak beef short ribs kevin ground round t-bone. Doner short loin venison, shank kevin ribeye pork belly pastrami. Turkey picanha jerky short loin. Tenderloin meatball ground round, cow pork belly shoulder doner turkey.</p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="row">
+								<div class="col-md-6 col-md-offset-2">
+									<div class="panel panel-default">
+										<div class="panel-body">
+											<p>Bacon ipsum dolor amet prosciutto chuck beef jowl pig boudin. Turkey bresaola ball tip prosciutto turducken tongue shoulder drumstick pork chop strip steak beef short ribs kevin ground round t-bone. Doner short loin venison, shank kevin ribeye pork belly pastrami. Turkey picanha jerky short loin. Tenderloin meatball ground round, cow pork belly shoulder doner turkey.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6 col-md-offset-2">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h3 class="panel-title">I'm new. Sign up</h3>
+										</div>
+										<div class="panel-body">
+											<p>Bacon ipsum dolor amet prosciutto chuck beef jowl pig boudin. Turkey bresaola ball tip prosciutto turducken tongue shoulder drumstick pork chop strip steak beef short ribs kevin ground round t-bone. Doner short loin venison, shank kevin ribeye pork belly pastrami. Turkey picanha jerky short loin. Tenderloin meatball ground round, cow pork belly shoulder doner turkey.</p>
+										</div>
+									</div>
+							  	</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section> <!-- /.MAIN -->
+		</div> <!-- ./wrapper -->
+
+		<!-- FOOTER -->
+		<footer>
+			<!-- FOOTER-BAR -->
+	        <div id="footer-bar">
+	            <div class="container">
+	                <div class="row">
+	                    <div class="col-md-12">
+	                        <p><strong>Application Name</strong></p>
+	                        <p>Copyright &copy; 2015 <a href="http://hookins.net">Daniel Hookins</a></p>
+	                    </div>
+	                </div> <!-- /.row -->
+	            </div> <!-- /.container -->
+	        </div> <!-- /.FOOTER-BAR -->
+		</footer> <!-- /.FOOTER -->
 
 		<!-- SCRIPTS -->
-		<script src="{{ URL::asset('_/js/bootstrap.js') }}"></script>
-		<script src="{{ URL::asset('_/js/login-signup.js') }}"></script>
+		<script src="{{ asset('_/js/bootstrap.js') }}"></script>
+		<script src="{{ asset('_/js/login-signup.js') }}"></script>
 	</body>
 </html>
