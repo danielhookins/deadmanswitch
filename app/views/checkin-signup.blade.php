f5acaf14070d47792323e3067451fbf03e7d15be
Daniel Hookins <daniel@hookins.net>
2015-01-22 12:46:23 +1030

    Changed copy on checkin-signup page

--- app/views/checkin-signup.blade.php
+++ app/views/checkin-signup.blade.php
@@ -39,7 +39,7 @@
 							<div class="col-md-9 col-md-offset-4 welcome-box">
 								<h2>Nice to see you.</h2>
 								<p>Check in to let the system know that everything is okay.</p>
-								<p>The Dead Man Switch is a fail-safe messaging application that will send your pre-written message if you fail to check in with the system within 7 days.</p>
+								<p>The Dead Man Switch is a fail-safe messaging application that will send your pre-written message if you fail to check in with the system every 7 days.</p>
 								<div class="text-center">
 									<a class="learn-more-btn" href="#">Learn more</a>
 								</div>
