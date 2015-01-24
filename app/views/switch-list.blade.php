05d948389a20496f20a07bf8a1eb4354186efe43
Daniel Hookins <daniel@hookins.net>
2015-01-24 13:56:29 +1030

    Added template for listing switches

--- app/views/switch-list.blade.php
+++ app/views/switch-list.blade.php
@@ -10,8 +10,42 @@ Your Switches
 @stop
 
 @section('main-content')
-<h2>Your Switches</h2>
-<p>Here is a list of your switches.</p>
+<div class="hover-table">
+	<h2 id="tables-hover-rows">Switches</h2>
+	<p>Here are all of your active and inactive switches.</p>
+	<div class="bs-example" data-example-id="hoverable-table">
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th></th>
+					<th>Label</th>
+					<th>To Email</th>
+					<th>Status</th>
+					<th></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row"></th>
+					<td>Test message</td>
+					<td>danhookins@gmail.com</td>
+					<td><a href="#">Active</a></td>
+					<td>[<a href="#">edit</a>]</td>
+					<td>[<a href="#">delete</a>]</td>
+				</tr>
+				<tr>
+					<th scope="row"></th>
+					<td><a class="btn btn-default" href="{{ URL::to('/switches/create') }}" role="button"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Create new switch</a></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>	
+			</tbody>
+		</table>
+	</div>
+</div>	
 @stop
 
 @section('breadcrumbs')
