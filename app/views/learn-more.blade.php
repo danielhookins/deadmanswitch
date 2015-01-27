bc069a214727bf556249f230c30bc8027c8c2832
Daniel Hookins <daniel@hookins.net>
2015-01-27 19:54:21 +1030

    Allow the use of different content (widgets) in the right column of the checkin-signup template

--- app/views/learn-more.blade.php
+++ app/views/learn-more.blade.php
@@ -10,3 +10,33 @@ How it works
 	<p>The Dead Man Switch is a fail-safe messaging application that will send your pre-written message if you fail to check in with the system every 7 days.</p>
 </div>
 @stop
+
+@section('right-column-content')
+<div class="row">
+	<div class="col-md-7 col-md-offset-2">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><strong>Sign me up!</strong></h3>
+			</div>
+			<div class="panel-body">
+				{{ Form::open(array('route' => 'user.store')) }}
+					<div class="form-group">
+						{{ Form::text('name', Input::old('name'), array('placeholder'=>'Enter your name', 'class'=>'form-control')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::text('email', Input::old('email'), array('placeholder'=>'Email', 'class'=>'form-control')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::password('password', array('placeholder'=>'Password', 'class'=>'form-control' )) }}
+					</div>
+					<!-- only need password once
+					<div class="form-group">
+						{{ Form::password('password_again', array('placeholder'=>'Password again', 'class'=>'form-control' )) }}
+					</div> -->
+					{{ Form::submit('Sign up to create your Dead Man Switch', ['class'=>'btn btn-success']) }}
+				{{ Form::close() }}
+			</div>
+		</div>
+  	</div>
+</div>
+@stop
--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -14,3 +14,56 @@ Check in or Sign up
 	</div>
 </div>
 @stop
+
+@section('right-column-content')
+<div class="row">
+	<div class="col-md-7 col-md-offset-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				{{ Form::open(array('route' => 'session.store')) }}
+					<div class="form-group">
+						{{ Form::text('email', Input::old('email'), array('placeholder'=>'Enter your email', 'class'=>'form-control')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::password('password', array('placeholder'=>'Password', 'class'=>'form-control' )) }}
+					</div>
+					<!--
+					<div class="checkbox">
+						<label>
+							{{ Form::checkbox('remember') }} Remember me
+						</label>
+					</div> -->
+					{{ Form::submit('Check in', ['class'=>'btn btn-primary']) }}
+				{{ Form::close() }}
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-7 col-md-offset-2">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><strong>I'm new.</strong> Sign up</h3>
+			</div>
+			<div class="panel-body">
+				{{ Form::open(array('route' => 'user.store')) }}
+					<div class="form-group">
+						{{ Form::text('name', Input::old('name'), array('placeholder'=>'Enter your name', 'class'=>'form-control')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::text('email', Input::old('email'), array('placeholder'=>'Email', 'class'=>'form-control')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::password('password', array('placeholder'=>'Password', 'class'=>'form-control' )) }}
+					</div>
+					<!-- only need password once
+					<div class="form-group">
+						{{ Form::password('password_again', array('placeholder'=>'Password again', 'class'=>'form-control' )) }}
+					</div> -->
+					{{ Form::submit('Sign up to create your Dead Man Switch', ['class'=>'btn btn-success']) }}
+				{{ Form::close() }}
+			</div>
+		</div>
+  	</div>
+</div>
+@stop
